dd3a96600149cdef2b3b642f17e956ed4df9dc45
chausknecht <chausknecht@nundp.com>
2022-02-02 12:47:02 +0100

    Improve parser and generator
    - parser can cope with interfaces too now
    - generator adds ``trueValue`` parameter for ``Boolean`` attributes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,13 +3,17 @@ import kotlin.io.path.Path
 
 data class Attribute(val name: String, val type: String) {
     companion object {
-        val matcher = "open var (.+): (.+)".toRegex()
+        val matcher = "var (.+): (.+)".toRegex()
     }
 }
 
 data class HtmlTag(val name: String, val attributes: List<Attribute>) {
     companion object {
-        val matcher = "class (\\S+) ".toRegex()
+        val matcher = "(class|interface) (\\S+) ".toRegex()
+        val tokenTriggers = setOf(
+            "public external abstract class HTML",
+            "public external interface HTML"
+        )
     }
 }
 
@@ -30,19 +34,26 @@ tailrec fun parseAttributes(
 
 tailrec fun parse(lines: List<String>, accu: List<HtmlTag> = emptyList()): List<HtmlTag> =
     if (lines.isEmpty()) accu
-    else if (lines.first().startsWith("public external abstract class HTML")) {
+    else if (HtmlTag.tokenTriggers.any { lines.first().startsWith(it) }) {
         val (attributes, skip) = parseAttributes(lines.drop(1))
-        val (tagName) = HtmlTag.matcher.find(lines.first())!!.destructured
+        val (_, tagName) = HtmlTag.matcher.find(lines.first())!!.destructured
         parse(lines.drop(skip), accu + HtmlTag(tagName, attributes))
     } else parse(lines.drop(1), accu)
 
 
 fun main(args: Array<String>) {
     println(buildString {
-        parse(Files.readAllLines(Path(args[0]))).forEach { tag ->
+        parse(Files.readAllLines(Path(args[0]))).filter { it.attributes.isNotEmpty() }.forEach { tag ->
+            appendLine()
+            appendLine("// ${tag.name} attributes")
             tag.attributes.forEach { (name, type) ->
-                appendLine("""fun Tag<${tag.name}>.$name(value: $type) = attr("$name", value)""")
-                appendLine("""fun Tag<${tag.name}>.$name(value: Flow<$type>) = attr("$name", value)""")
+                if (type == "Boolean") {
+                    appendLine("""fun Tag<${tag.name}>.$name(value: $type, trueValue: String = "") = attr("$name", value, trueValue)""")
+                    appendLine("""fun Tag<${tag.name}>.$name(value: Flow<$type>, trueValue: String = "") = attr("$name", value, trueValue)""")
+                } else {
+                    appendLine("""fun Tag<${tag.name}>.$name(value: $type) = attr("$name", value)""")
+                    appendLine("""fun Tag<${tag.name}>.$name(value: Flow<$type>) = attr("$name", value)""")
+                }
             }
         }
     })
